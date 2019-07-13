75c2ee01f192cf71689627d0f9138769a32699c6
devesha1 <deveshideal2005@gmail.com>
2019-07-13 21:37:42 +0530

    Added services

---
+++ MyPodDemo/MyPodDemo/Services.swift
@@ -0,0 +1,15 @@
+//
+//  Services.swift
+//  MyPodDemo
+//
+//  Created by Devesh Sharma on 13/07/19.
+//  Copyright © 2019 Devesh Sharma. All rights reserved.
+//
+
+import Foundation
+
+public class Services{
+    static public func doSomeThing(){
+        print("Did something......")
+    }
+}
